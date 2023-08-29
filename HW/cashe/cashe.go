5fde9f3306d48de54cf6e14687ec98d72d012960
Artem Suslko <artem.susalko@vrpconsulting.com>
2023-08-29 17:41:52 +0300

    add validation

--- HW/cashe/cashe.go
+++ HW/cashe/cashe.go
@@ -1,5 +1,7 @@
 package cashe
 
+import "errors"
+
 type Cashe struct {
 	cashMap map[string]interface{}
 }
@@ -10,14 +12,28 @@ func New() *Cashe {
 	}
 }
 
-func (c *Cashe) Set(key string, value interface{}) {
+func (c *Cashe) Set(key string, value interface{}) error {
+	exist := c.cashMap[key]
+	if exist != nil {
+		return errors.New("Запись с ключем [" + key + "] существует")
+	}
 	c.cashMap[key] = value
+	return nil
 }
 
-func (c *Cashe) Get(key string) interface{} {
-	return c.cashMap[key]
+func (c *Cashe) Get(key string) (interface{}, error) {
+	exist := c.cashMap[key]
+	if exist == nil {
+		return nil, errors.New("Запись с таким ключем [" + key + "] не существует")
+	}
+	return c.cashMap[key], nil
 }
 
-func (c *Cashe) Delete(key string) {
+func (c *Cashe) Delete(key string) error {
+	exist := c.cashMap[key]
+	if exist == nil {
+		return errors.New("Запись с таким ключем [" + key + "] не существует")
+	}
 	delete(c.cashMap, key)
+	return nil
 }
